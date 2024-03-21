ad6232a73d95197f1e83ef27e8b9aaa0653a9989
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-21 10:59:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// used of if, else if, and else statements
+var temperature = 39;
+//whem temp between 40 & 30
+if (temperature >= 30 && temperature <= 40) {
+    console.log("It's too hot");
+    //whem temp between 30 & 20
+}
+else if (temperature >= 20 && temperature < 30) {
+    console.log("The weather is pleasant");
+    //whem temp between 20 & 10
+}
+else if (temperature >= 10 && temperature < 20) {
+    console.log("It's too cold");
+    //else condition 
+}
+else {
+    console.log("Today is windy weather");
+}
